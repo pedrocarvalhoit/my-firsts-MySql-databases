2f3faed348918603cbee633520909b37e47792f7
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-08-29 10:19:13 +0100

    DB clear

--- MyFirst.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM tbproduto;
-
-SELECT * FROM tbproduto WHERE PRODUTO = '1042712';
-
-SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro';
-
-SELECT * FROM tbproduto WHERE SABOR = 'Limão';
-
-UPDATE tbproduto SET sabor = 'Cítricus' WHERE sabor = 'Citurs';
