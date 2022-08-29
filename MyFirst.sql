14079f0382d539b7b61ee639b03e2a255319187c
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-08-29 09:32:03 +0100

    My first sql code

---
+++ MyFirst.sql
@@ -0,0 +1,9 @@
+SELECT * FROM tbproduto;
+
+SELECT * FROM tbproduto WHERE PRODUTO = '1042712';
+
+SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro';
+
+SELECT * FROM tbproduto WHERE SABOR = 'Limão';
+
+UPDATE tbproduto SET sabor = 'Cítricus' WHERE sabor = 'Citurs';
