aa20f615e50a9dff0b6f196caf39ae667d044d81
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-08-29 11:57:49 +0100

    create

--- create.sql
+++ create.sql
@@ -1,3 +1,16 @@
 /* Database with best exercises chosen by muscle activation */
 
-create DATABASE strengthTraining;
+CREATE DATABASE strengthTraining;
+
+/*Adding exercises
+ID = First two letters of each word in the name */
+USE strengthtraining;
+
+CREATE TABLE tbexercises
+(id VARCHAR(15),
+muclegroup VARCHAR(30),
+title VARCHAR(50),
+equipment VARCHAR(30),
+muscle_activation_percentage DOUBLE(10,0)
+);
+
